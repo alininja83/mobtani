ea6ba985011e420074291695d738c715d304759e
unknown <alininja6672@gmail.com>
2024-04-21 15:29:21 +0330

    new pages

---
+++ about.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="fa">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>درباره ما</title>
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+
+<body dir="rtl">
+    <header>
+        <?php
+        include ('view/navbar.php');
+        ?>
+    </header>
+    <div class="container text-right" style="directon: rtl !important">
+        <div class="border shadow-lg bg-success mt-3 p-3 rounded-lg">
+            <h1 class="text-center" style="color: orange;">وبلاگ سایت</h1>
+        </div>
+        <div class="row mt-3">
+            <div class="col-md-4 mb-3">
+                <div class="border bg-primary">
+                    <div class="text-center text-white">
+                        <h2 class="display-5 font-wight-bold">عنوان</h2>
+                    </div>
+                    <div class="text-white p-3 text-justify">
+                    لورم ایپسوم متن ساخ طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسراحی اساسا مورد استفاده قرار گیرد.
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="border bg-primary">
+                    <div class="text-center text-white">
+                        <h2 class="display-5 font-wight-bold">عنوان</h2>
+                    </div>
+                    <div class="text-white p-3 text-justify">
+                    لورم ایپسوم متن ساخ طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسراحی اساسا مورد استفاده قرار گیرد.
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="border bg-primary">
+                    <div class="text-center text-white">
+                        <h2 class="display-5 font-wight-bold">عنوان</h2>
+                    </div>
+                    <div class="text-white p-3 text-justify">
+                    لورم ایپسوم متن ساخ طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسراحی اساسا مورد استفاده قرار گیرد.
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="border bg-primary">
+                    <div class="text-center text-white">
+                        <h2 class="display-5 font-wight-bold">عنوان</h2>
+                    </div>
+                    <div class="text-white p-3 text-justify">
+                    لورم ایپسوم متن ساخ طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسراحی اساسا مورد استفاده قرار گیرد.
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="border bg-primary">
+                    <div class="text-center text-white">
+                        <h2 class="display-5 font-wight-bold">عنوان</h2>
+                    </div>
+                    <div class="text-white p-3 text-justify">
+                    لورم ایپسوم متن ساخ طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسراحی اساسا مورد استفاده قرار گیرد.
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="border bg-primary">
+                    <div class="text-center text-white">
+                        <h2 class="display-5 font-wight-bold">عنوان</h2>
+                    </div>
+                    <div class="text-white p-3 text-justify">
+                    لورم ایپسوم متن ساخ طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسراحی اساسا مورد استفاده قرار گیرد.
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="border bg-primary">
+                    <div class="text-center text-white">
+                        <h2 class="display-5 font-wight-bold">عنوان</h2>
+                    </div>
+                    <div class="text-white p-3 text-justify">
+                    لورم ایپسوم متن ساخ طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسراحی اساسا مورد استفاده قرار گیرد.
+                    </div>
+                </div>
+            </div>
+           
+        </div>
+    </div>
+    <div style="border : solid orange 3px; padding:5px;text-align:center;">
+    <a href="index.php" style=" color: black;">بازگشت</a>
+    </div>
+</body>
---
+++ view/navbar.php
@@ -0,0 +1,15 @@
+<div class="navbar">
+    <div class="container">
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link" href="about.php">درباره ما</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="view/blog/new-blog.php">وبلاگ</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">تماس با ما</a>
+            </li>
+        </ul>
+    </div>
+</div> 
